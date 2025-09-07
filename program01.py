c4c5ead20ccd25f44fb8c802ebe14ba42ff915d9
Yolanda Gunter <yemgunter@gmail.com>
2025-09-07 16:52:13 -0500

    Update program01.py
    code Requirements 1, 2 and 3

---
+++ program01.py
@@ -0,0 +1,21 @@
+# program01.py
+
+print('\nRequirement 1\n')
+print('This is Program 1 - Yolanda Gunter\n')
+
+print('Requirement 2\n')
+print('Please enter the information rquested.\n')
+name = input('Last name: ')
+hometown = input('Hometown: ')
+occupation = input('Occupation: ')
+hobby = input('Hobby: ')
+
+print('\nRequirement 3\n')
+print('Hello {}!'.format(name))
+print('From {}.'.format(hometown))
+print('I hope you like being a(n) {}.'.format(occupation))
+print('{} sounds like an interesting hobby.'.format(hobby))
+
+
+
+      
