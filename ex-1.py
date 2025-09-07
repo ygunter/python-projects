9dab48a1b7ea37b4f3fb7fa93d06ac0fb6eeff7a
Yolanda Gunter <yemgunter@gmail.com>
2025-09-07 00:02:23 -0500

    Create ex-1.py file
    Begin syntax refreshers

---
+++ ex-1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python37
+# Line above useful for Unix-based
+# operating systems like Linux and OSX.
+
+# ex-1py
+
+print("\nPython College")
+print("777 Serpentine Drive")
+print("Austin, TX 78758\n")
+message = "A fun place to code!\n"
+print(message)
+
+pups = 2
+kittens = 5
+
+pets = pups + kittens
+
+print("I have this many pets: ", pets)
+
+print("\nI have {} pets.".format(pets))
